49fa10e32acdd984a9125531faa20fb7ad88d7db
LazuardiAdityaPratamaPutra <lazuardiaditya17@gmail.com>
2025-08-17 20:39:27 +0700

    First Commit

---
+++ script.js
@@ -0,0 +1,38 @@
+alert('Selamat datang di Game Suit Jawa Sederhana');
+
+var suit = true;
+
+while (suit) {
+  // Menangkap Pilihan Player
+  var p = prompt('Pilih : Gajah, Orang, Semut').toLowerCase();
+
+  // Menangkap Pilihan Computer
+  var comp = Math.random();
+  
+  if ( comp <= 0.33) {
+    comp = 'gajah';
+  }else if ( comp >= 0.34 && comp <= 0.66){
+    comp = 'orang';
+  }else {
+    comp = 'semut';
+  }
+
+  // Menentukan Rules
+  var hasil = "";
+git 
+  if ( p == comp ) {
+    hasil = 'SERI';
+  }else if ( p == 'gajah'){
+    hasil = ( comp == 'orang' ) ? 'MENANG!' : 'KALAH!';
+  }else if ( p == 'orang' ){
+    hasil = ( comp == 'gajah' ) ? 'KALAH!' : 'MENANG!';
+  }else if ( p == 'semut' ){
+    hasil = ( comp == 'gajah' ) ? 'MENANG!' : 'KALAH!';
+  }
+
+  // Menampilkan Hasil
+  alert('Kamu memilih : ' + p + ' dan Komputer memilih ' + comp + ' hasilnya kamu ' + hasil);
+
+  suit = confirm('Mau coba main lagi?');
+}
+alert('Terima Kasih sudah mencoba Game Sederhana ini!!!');
